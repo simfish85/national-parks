b9697d7d80782ff5de32e5bdb98609a08b3bc9a4
Simon Fisher <simfish85@googlemail.com>
2018-01-19 15:46:36 +0000

    Updated dependency on openssh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=national-parks
 pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
 pkg_origin=simfish85
-pkg_version=6.3.0
+pkg_version=6.3.1
 pkg_maintainer="Bill Meyer <bill@chef.io>"
 pkg_license=('Apache-2.0')
-pkg_deps=(core/tomcat8 core/jre8)
+pkg_deps=(core/tomcat8 core/jre8 core/openssh)
 pkg_build_deps=(core/jdk8/8u131 core/maven)
 pkg_svc_user="root"
 pkg_binds=(
